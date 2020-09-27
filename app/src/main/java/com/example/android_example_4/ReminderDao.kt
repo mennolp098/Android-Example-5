ef07ab9416bc973e43d99d31fc85c78fce0fed6a
Menno <run_jongejan@hotmail.com>
2020-09-27 19:53:46 +0200

    Initial setup

---
+++ app/src/main/java/com/example/android_example_4/ReminderDao.kt
@@ -0,0 +1,21 @@
+package com.example.android_example_4
+
+import androidx.room.*
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminderTable")
+    suspend fun getAllReminders(): List<Reminder>
+
+    @Insert
+    suspend fun insertReminder(reminder: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminder: Reminder)
+
+    @Update
+    suspend fun updateReminder(reminder: Reminder)
+
+}
+
